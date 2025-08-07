e51832d54da17ecb66be037dfcbfe363098e6187
Diogo Fernando de Melo Sales <diogomsales314@gmail.com>
2025-08-07 14:10:38 -0300

    count and say exercise fnished with logs

--- src/main/java/org/com/Main.java
+++ src/main/java/org/com/Main.java
@@ -6,27 +6,22 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        Main.processNumber("111221");
-    }
-
-    public String countAndSay(int n) {
-
-        return "";
+        System.out.printf("Input: %-8s Output: %-8s Expect: %s%n", "21", processNumber("21"), "1211");
+        System.out.printf("Input: %-8s Output: %-8s Expect: %s%n", "1211", processNumber("1211"), "111221");
+        System.out.printf("Input: %-8s Output: %-8s Expect: %s%n", "111221", processNumber("111221"), "312211");
+        System.out.printf("Input: %-8s Output: %-8s Expect: %s%n", "312211", processNumber("312211"), "13112221");
     }
 
     public static String processNumber(String number) {
         HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
-        String countAndSay = "";
-        // sliding window []
-        //[1,2,1,1]
+        StringBuilder countAndSay = new StringBuilder();
         String[] split = number.split("");
-
         for (int i = 0; i < split.length; i++) {
             if(i < split.length - 1 && Objects.equals(split[i], split[i+1])) {
                //increase window
                 //validate window boundary
                 for (int j = i; j < split.length; j++) {
-                    if(!Objects.equals(split[j], split[j + 1])) {
+                    if(j == split.length-1 || !Objects.equals(split[j], split[j + 1])) {
                         stringIntegerHashMap.put(split[j], j-i+1);
                         i = j;
                         break;
@@ -36,10 +31,9 @@ public class Main {
                 stringIntegerHashMap.put(split[i], 1);
             }
 
-            countAndSay += stringIntegerHashMap.get(split[i]) + split[i];
+            countAndSay.append(stringIntegerHashMap.get(split[i])).append(split[i]);
             stringIntegerHashMap.clear();
         }
-        System.out.println(stringIntegerHashMap);
-        return null;
+        return countAndSay.toString();
     }
 }
